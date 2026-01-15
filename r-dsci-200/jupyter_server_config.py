a90a7afe592fff14dc1e0062c4b0d5f810f0130e
Brian Kim <brian.kim.2011@gmail.com>
2026-01-15 10:04:09 -0800

    Allow JupyterHub apps to open a frame

--- r-dsci-200/jupyter_server_config.py
+++ r-dsci-200/jupyter_server_config.py
@@ -47,3 +47,10 @@ c.AsyncContentsManager.allow_hidden = True
 ## Allow access to hidden files
 #  See also: ContentsManager.allow_hidden
 c.AsyncFileContentsManager.allow_hidden = True
+
+# Allow JupyterHub apps to open a frame
+c.ServerApp.tornado_settings = {
+    'headers': {
+        'Content-Security-Policy': "frame-ancestors 'self'"
+    }
+}
